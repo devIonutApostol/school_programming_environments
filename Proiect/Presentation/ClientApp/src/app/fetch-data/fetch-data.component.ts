1bcc034c142e880e49f42117de4b25b15da1601c
Ionut Apostol <ionut.apostol@connatix.com>
2023-01-08 01:48:27 +0200

    tst

--- Proiect/Presentation/ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ Proiect/Presentation/ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -6,18 +6,12 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './fetch-data.component.html'
 })
 export class FetchDataComponent {
-  public forecasts: WeatherForecast[] = [];
+  public accounts: any;
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<WeatherForecast[]>(baseUrl + 'weatherforecast').subscribe(result => {
-      this.forecasts = result;
+    http.get(baseUrl + 'accounts/list').subscribe(result => {
+      this.accounts = result;
     }, error => console.error(error));
   }
 }
 
-interface WeatherForecast {
-  date: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
-}
---
+++ Proiect/Presentation/Controllers/AccountsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Presentation.Data;
+using Presentation.Models;
+
+namespace Presentation.Controllers;
+
+[Authorize]
+[ApiController]
+[Route("[controller]")]
+public class AccountsController : Controller
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public AccountsController(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    
+    public async Task<Account> Get(Guid accountId)
+    {
+        return await _dbContext.Accounts.FindAsync(accountId);
+    }
+
+    public async Task<IEnumerable<Account>> List()
+    {
+        return await _dbContext.Accounts.ToListAsync();
+    }
+}
